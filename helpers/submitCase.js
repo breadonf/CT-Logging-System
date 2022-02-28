9def5ccbfdace55f09408985a1b252e724936c74
Will Lee <willlee19951009@gmail.com>
2022-02-28 16:46:12 +0800

    mutation

--- helpers/submitCase.js
+++ helpers/submitCase.js
@@ -1,11 +1,22 @@
-import { useMutation } from 'react-query'
-
 const graphQLAPI = process.env.NEXT_PUBLIC_GRAPHQL;
 
-const SubmitCase = () => {
-    const mutation = useMutation(newCase => {
-        return axios.post(graphQLAPI, newCase)
-    })
+const submitCase = async (mutation, data = {}) => {
+  const query = JSON.stringify({
+    query: mutation,
+    variables: data,
+  });
 
-    return 
-}
+  const headers = { "Content-Type": "application/json" };
+
+  const res = await fetch(graphQLAPI, {
+    method: "POST",
+    headers,
+    body: query,
+  });
+
+  const json = await res.json();
+
+  return json.data;
+};
+
+export default submitCase;
