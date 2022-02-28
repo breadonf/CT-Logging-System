133c978514f81991c6d6cd3da856c95591487e78
Will Lee <willlee19951009@gmail.com>
2022-02-28 16:46:12 +0800

    mutation

--- queries/mutations.js
+++ queries/mutations.js
@@ -2,10 +2,19 @@ import { useMutation } from "react-query";
 import { useRouter } from "next/router";
 // need custom date scalar?
 // change type to contrastType?
-
+export const createCTrecord = `
+  mutation Mutation($data: create_CT_input!) {
+    create_CT_item(data: $data) {
+      count
+      PID
+      protocol
+      status
+    }
+  }
+`;
 export const submitCaseContainer = (props) => {
   const [mutate] = useMutation(submitCaseMutation);
-  const router = useRouter()
+  const router = useRouter();
   async function submit(values) {
     const { data } = await mutate({
       variables: values,
