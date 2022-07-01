d5d5152f9e2f038d8048756560f59d4a5ffad9cc
Will Lee <you@example.com>
2022-07-02 02:38:31 +0800

    cardiac form created not finished

--- components/Forms/CardiacForm/index.js
+++ components/Forms/CardiacForm/index.js
@@ -1,14 +1,107 @@
 import React from "react";
-import { Paper, Typography } from "@mui/material";
+import { Formik, Form } from "formik";
+import { Container, Paper, Grid, Typography } from "@mui/material";
 
-function CardiacForm() {
-  return (
-    <Paper elevation={12} sx={{ px: 3, py: 5, bgcolor: "#F0F3BD" }}>
-      <Typography align="center" variant="h1">
-        Coming Soon
-      </Typography>
-    </Paper>
+import CircularProgress from "@mui/material/CircularProgress";
+import { useQuery } from "react-query";
+
+import LocalizationProvider from "@mui/lab/LocalizationProvider";
+import AdapterDateFns from "@mui/lab/AdapterDateFns";
+import FORM_VALIDATION from "./ValidationSchema";
+import PatientDetail from "./patientDetail";
+
+import { getHomepageData } from "../../../queries/queries";
+
+function CardiacForm({ data, handleSubmit }) {
+  const { data: autocompleteOptions, isSuccess, isLoading } = useQuery(
+    "autocompleteOptions",
+    async () => await getHomepageData()
   );
+
+  if (isLoading) {
+    return (
+      <Grid container>
+        <Grid item xs={12}>
+          <Container maxWidth="lg">
+            <Paper
+              elevation={12}
+              sx={{
+                p: 3,
+                height: "85vh",
+                bgcolor: "#F0F3BD",
+                overflowY: "auto",
+              }}
+            >
+              <Grid
+                container
+                spacing={2}
+                sx={{ py: 5 }}
+                direction="column"
+                justifyContent="center"
+                alignItems="center"
+              >
+                <Grid item xs={12}>
+                  <CircularProgress size="25vh" />
+                </Grid>
+              </Grid>
+            </Paper>
+          </Container>
+        </Grid>
+      </Grid>
+    );
+  }
+  if (isSuccess) {
+    return (
+      <LocalizationProvider dateAdapter={AdapterDateFns}>
+        <Grid container>
+          <Grid item xs={12}>
+            <Container maxWidth="lg">
+              <Formik
+                initialValues={data}
+                validationSchema={FORM_VALIDATION}
+                onSubmit={handleSubmit}
+              >
+                {(formik) => (
+                  <Form>
+                    <Grid container spacing={2} sx={{ height: "90vh", py: 3 }}>
+                      <Grid item xs={12}>
+                        <Paper
+                          elevation={12}
+                          sx={{
+                            p: 3,
+                            height: "85vh",
+                            bgcolor: "#F0F3BD",
+                            overflowY: "auto",
+                          }}
+                        >
+                          <Grid item xs={12}>
+                            <Typography
+                              variant="h3"
+                              align="center"
+                              color="#093A3E"
+                            >
+                              Cardiac Case Setup Log Form
+                            </Typography>
+                          </Grid>
+                          <PatientDetail />
+                        </Paper>
+                      </Grid>
+                    </Grid>
+                  </Form>
+                )}
+              </Formik>
+            </Container>
+          </Grid>
+        </Grid>
+      </LocalizationProvider>
+    );
+  } else {
+    return (
+      <>
+        <div>not loading or success</div>
+      </>
+    );
+  }
 }
 
 export default CardiacForm;
--- pages/Forms/CardiacCases.js
+++ pages/Forms/CardiacCases.js
@@ -1,9 +1,72 @@
 import React from "react";
 import CardiacForm from "../../components/Forms/CardiacForm";
-
+import INITIAL_FORM_STATE from "../../components/Forms/CardiacForm/InitialFormState";
+import preprocessor from "../../helpers/preprocessor";
+import { createCTrecord } from "../../queries/mutations";
+import { useMutation } from "react-query";
+import setData from "../../helpers/setData";
+import CircularProgress from "@mui/material/CircularProgress";
+import Box from "@mui/material/Box";
+import { useRouter } from "next/router";
+import Head from "next/head";
 
 function CardiacCases() {
-  return <CardiacForm />;
+  const router = useRouter();
+  const mutation = useMutation(
+    async (newFormData) => {
+      await setData(createCTrecord, { data: newFormData });
+    },
+    {
+      mutationKey: "createCTitem",
+      onSuccess: (data, variables, context) => {
+        console.log("onSucces", data, variables);
+      },
+    }
+  );
+  const handleSubmit = async (values, { setSubmitting }) => {
+    console.log(values);
+    //   const modifiedValues = preprocessor(values);
+    //   await new Promise((r) => setTimeout(r, 750));
+
+    //   mutation.mutate(
+    //     { ...modifiedValues },
+    //     {
+    //       onSuccess: async (res) => {
+    //         alert(
+    //           `Success, your Ct records for ${modifiedValues.PID} at ${modifiedValues.Date} is created`
+    //         );
+    //         router.push("/Table");
+    //       },
+    //       onError: async (err, varia) => {
+    //         console.log("onError", varia);
+    //       },
+    //     }
+    //   );
+    //   if (mutation.isError) {
+    //     console.log(
+    //       "Fail,🚀 ~ file: index.js ~ line 74 ~ RoutineForm ~ mutation",
+    //       mutation.error
+    //     );
+    //   }
+    //   if (mutation.onSettled) {
+    //     <Box sx={{ display: "flex" }}>
+    //       <CircularProgress />
+    //     </Box>;
+    //   }
+    // };
+    // if (mutation.isSuccess) {
+    // }
+  };
+  return (
+    <>
+      <Head>
+        <title>CT Cardiac record</title>
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+      <CardiacForm data={INITIAL_FORM_STATE} handleSubmit={handleSubmit} />
+    </>
+  );
 }
 
 export default CardiacCases;
