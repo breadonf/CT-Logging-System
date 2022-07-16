c784e9b0f72c1d01836b48b5140f85fb553b2c97
breadonf <72241739+breadonf@users.noreply.github.com>
2022-07-17 00:05:33 +0800

    minor changes on log history

--- components/Logbook/History/MessageCard.js
+++ components/Logbook/History/MessageCard.js
@@ -13,75 +13,69 @@ import parse from "html-react-parser";
 
 const MessageCard = ({ messageDetail }) => {
   return (
-    <>
-      {messageDetail.active ? (
-        <Grid item xs={12}>
-          <Link href={"/Logbook"}>
-            <Card raised>
-              <CardActionArea>
-                <CardContent>
-                  <Grid container spacing={2}>
-                    <Grid
-                      item
-                      xs={2}
-                      sx={{
-                        borderRight: 1,
-                        mt: 2,
-                      }}
-                    >
-                      <Typography>{messageDetail.category}</Typography>
-                      {messageDetail.important ? (
-                        <Typography variant="h1" align="center" color="#8b0000">
-                          <FmdBadIcon fontSize="string" />
-                        </Typography>
-                      ) : (
-                        <></>
-                      )}
+    <Grid item xs={12}>
+      <Link href={"/Logbook"}>
+        <Card raised>
+          <CardActionArea>
+            <CardContent>
+              <Grid container spacing={2}>
+                <Grid
+                  item
+                  xs={2}
+                  sx={{
+                    borderRight: 1,
+                    mt: 2,
+                  }}
+                >
+                  <Typography>{messageDetail.category}</Typography>
+                  {messageDetail.important ? (
+                    <Typography variant="h1" align="center" color="#8b0000">
+                      <FmdBadIcon fontSize="string" />
+                    </Typography>
+                  ) : (
+                    <></>
+                  )}
+                </Grid>
+                <Grid item xs={10}>
+                  <Grid container>
+                    <Grid item xs={3}>
+                      <Typography>
+                        Created at: {messageDetail.inputDate}
+                      </Typography>
                     </Grid>
-                    <Grid item xs={10}>
-                      <Grid container>
-                        <Grid item xs={3}>
-                          <Typography>
-                            Created at: {messageDetail.inputDate}
-                          </Typography>
-                        </Grid>
-                        <Grid item xs={3}>
-                          <Typography>
-                            Effective from: {messageDetail.effectiveDate}
-                          </Typography>
-                        </Grid>
-                        <Grid item xs={3}>
-                          <Typography align="right">
-                            Message from: {messageDetail.inputUser}
-                          </Typography>
-                        </Grid>
-                        <Grid item xs={3}>
-                          <Typography align="right">
-                            Originated by: {messageDetail.originatedBy}
-                          </Typography>
-                        </Grid>
-                        <Grid item xs={12}>
-                          <Divider />
-                          {parse(messageDetail.htmlMessage)}
-                        </Grid>
-                        <Grid item xs={12}>
-                          <Divider />
-                          <Typography>
-                            Follow Up Comments: {messageDetail.comments}
-                          </Typography>
-                        </Grid>
-                      </Grid>
+                    <Grid item xs={3}>
+                      <Typography>
+                        Effective from: {messageDetail.effectiveDate}
+                      </Typography>
+                    </Grid>
+                    <Grid item xs={3}>
+                      <Typography align="right">
+                        Message from: {messageDetail.inputUser}
+                      </Typography>
+                    </Grid>
+                    <Grid item xs={3}>
+                      <Typography align="right">
+                        Originated by: {messageDetail.originatedBy}
+                      </Typography>
+                    </Grid>
+                    <Grid item xs={12}>
+                      <Divider />
+                      {parse(messageDetail.htmlMessage)}
+                    </Grid>
+                    <Grid item xs={12}>
+                      <Divider />
+                      <Typography>
+                        Follow Up Comments: {messageDetail.comments}
+                      </Typography>
                     </Grid>
                   </Grid>
-                </CardContent>
-              </CardActionArea>
-            </Card>
-          </Link>
-        </Grid>
-      ) : (
-        <></>
-      )}
-    </>
+                </Grid>
+              </Grid>
+            </CardContent>
+          </CardActionArea>
+        </Card>
+      </Link>
+    </Grid>
   );
 };
 
