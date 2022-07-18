1006661424540e256696ba2762397e2a86ddc8ee
breadonf <72241739+breadonf@users.noreply.github.com>
2022-07-18 21:47:03 +0800

    added submission function ,untested

--- pages/Message/Form/index.js
+++ pages/Message/Form/index.js
@@ -2,15 +2,54 @@ import React from "react";
 import Head from "next/head";
 import MessageForm from "../../../components/Message/MessageForm";
 import preprocessorMessage from "../../../components/Message/MessageForm/preprocessorMessage";
+import { useRouter } from "next/router";
+import { useMutation } from "react-query";
+import setData from "../../../helpers/setData";
+import { createMessage } from "../../../queries/mutations";
+import Box from "@mui/material";
+import { CircularProgress } from "@mui/material";
 
 function MessageFormPage() {
+  const router = useRouter;
+  const mutation = useMutation(
+    async (newFormData) => {
+      await setData(createMessage, {data: newFormData});
+    },
+    {mutationKey: "createMessage",
+    onSuccess: (data, variables, context) => {
+      console.log("onSuccess", data, variables);
+    }
+  }
+  );
   const handleSubmit = (values, { setSubmitting }) => {
-    setTimeout(() => {
-      const processedValues = preprocessorMessage(values);
-      alert(JSON.stringify(processedValues, null, 2));
-      console.log(processedValues);
-      setSubmitting(false);
-    }, 1000);
+      const modifiedValues = preprocessorMessage(values);
+      await new Promise((r) => setTimeout(r, 500));
+
+      mutation.mutate(
+        {...modifiedValues},
+        {
+          onSuccess: async (res) => {
+            router.push("/Message");
+          },
+          onError: async (err, variables) => {
+            console.log("onError", err, variables)
+          }
+        }
+      );
+
+      if (mutation.isError) {
+        console.log(
+          "failed",
+          mutation.error
+        );
+      }
+      if (mutation.onSettled) {
+        <Box sx={{ display: "flex" }}>
+          <CircularProgress />
+        </Box>;
+      }
+  };
+  if (mutation.isSuccess) {
   };
 
   return (
