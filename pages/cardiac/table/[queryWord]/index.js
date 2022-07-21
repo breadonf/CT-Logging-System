a669db3c93f15d2cf62980d3ee953ae8dd91cae4
Will Lee <willlee19951009@gmail.com>
2022-07-21 17:51:44 +0800

    minor

--- pages/cardiac/table/[queryWord]/index.js
+++ pages/cardiac/table/[queryWord]/index.js
@@ -1,51 +1,44 @@
-import React from "react";
+import { useEffect, useState } from "react";
 import Head from "next/head";
 import { useQuery } from "react-query";
 import { useRouter } from "next/router";
-import { getCardiacSetupByID } from "../../../../queries/queries";
-import {
-  Paper,
-  Grid,
-  Box,
-  Container,
-  Typography,
-  Divider,
-} from "@mui/material";
-import PatientDetail from "../../../../components/CardiacForm/PatientDetail";
-import CardiacSetup from "../../../../components/CardiacForm/CardiacSetup";
-import Buttons from "../../../../components/CardiacForm/Buttons";
+import { getCardiacSetupRecordBySearch } from "../../../../queries/queries";
+import { Paper, Grid, Container } from "@mui/material";
 import { LoadingSpinner } from "../../../../components/Forms/CardiacForm/LoadingSpinner";
-
-export default function CardiacSetupViewer() {
+import { RecordTableHeaders } from "../../../../components/Table/CardiacRecordTableHeader";
+import Filters from "../../../../components/Table/Filters";
+import TableMaterial from "../../../../components/Table/TableMaterial";
+export default function CardiacSetupTable() {
   const router = useRouter();
   const { queryWord } = router.query;
+  const [pageNumber, setPageNumber] = useState(1);
   const {
-    data,
+    data: records,
     isLoading: isQueryLoading,
     isSuccess: isQuerySuccess,
     isError,
     error,
   } = useQuery(
-    ["CardiacSetupByID", queryWord],
+    ["CardiacSetupRecordBySearch", queryWord],
     async () => await getCardiacSetupRecordBySearch(queryWord),
     { retry: true }
   );
-  console.log(data);
-  if (isQueryLoading || !isQuerySuccess || !data) {
+  console.log(records);
+  if (isQueryLoading || !isQuerySuccess || !records) {
     return <LoadingSpinner />;
   }
   if (isError) {
     return <></>;
   }
-  if (isQuerySuccess && data) {
-    const { cardiacCT_by_id } = data;
+  if (isQuerySuccess && records) {
+    const { cardiacCT_by_id } = records;
     if (cardiacCT_by_id === null) {
       return <h2>No such exam</h2>;
     }
     return (
       <Grid spacing={2} sx={{ py: 5 }} container>
         <Head>
-          <title>{`Records of ${cardiacCT_by_id?.PID} at ${cardiacCT_by_id.date_func.year}-${cardiacCT_by_id.date_func.month}-${cardiacCT_by_id.date_func.day}`}</title>
+          <title>{`Records of ${cardiacCT_by_id?.PID}`}</title>
           <link rel="icon" href="/favicon.ico" />
           <meta name="viewport" content="initial-scale=1, width=device-width" />
         </Head>
@@ -63,11 +56,12 @@ export default function CardiacSetupViewer() {
               <Filters />
               <TableMaterial
                 setPageNumber={setPageNumber}
-                records={records.CT}
+                records={records.cardiacCT}
                 isSuccess={isQuerySuccess}
                 isLoading={isQueryLoading}
                 pageNumber={pageNumber}
                 isPreviousData={isPreviousData}
+                columnHeaders={RecordTableHeaders}
                 paginationMode="client"
               />
             </Paper>
