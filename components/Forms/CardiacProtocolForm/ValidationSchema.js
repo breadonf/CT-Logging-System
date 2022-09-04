427875dbd432b9e6f7e7a80afb20e5df2e372be8
Will Lee <willlee19951009@gmail.com>
2022-09-04 16:50:48 +0800

    amending schema and validation for two form in one

--- components/Forms/CardiacProtocolForm/ValidationSchema.js
+++ components/Forms/CardiacProtocolForm/ValidationSchema.js
@@ -8,14 +8,20 @@ const FORM_VALIDATION = yup.object().shape({
     .test("is-HKID", "Not a valid HKID", (value) => IsHKID(value))
     .required("Required"),
   date: yup.string().typeError("Select a date").required("Required"),
+  readyForRecord: yup.boolean(),
   protocol: yup
     .array()
     .typeError("Not an array")
-    .min(1, "Required")
-    .max(
-      2,
-      "You can only choose 2 protocols, please submit another form if necessary"
-    ),
+    .when("readyForRecord", {
+      is: true,
+      then: (schema) =>
+        schema
+          .min(1, "Required")
+          .max(
+            2,
+            "You can only choose 2 protocols, please submit another form if necessary"
+          ),
+    }),
 });
 
 export default FORM_VALIDATION;
